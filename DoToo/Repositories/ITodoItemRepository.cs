ceb42ccdf7612049b50c2c17e84b3e3791189460
benji0295 <58788309+benji0295@users.noreply.github.com>
2024-07-29 21:48:23 -0400

    Created an interface and a repository to implement the interface. Initiated the use of SQLite to store to do list items.

---
+++ DoToo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoToo.Models;
+
+namespace DoToo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdated;
+
+        Task<List<TodoItem>> GetItemsAsync();
+        Task AddItemAsync(TodoItem item);
+        Task UpdateItemAsync(TodoItem item);
+        Task AddOrUpdateAsync(TodoItem item);
+    }
+}
